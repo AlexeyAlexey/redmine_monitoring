764a124c9969c72a9e215d61321c03d0302502af
Alexey Kondratenko <a.kondratenko@smileexpo.ru>
2016-11-23 16:45:23 +0200

    charts in one line

--- app/controllers/monitoring_results_controller.rb
+++ app/controllers/monitoring_results_controller.rb
@@ -8,12 +8,28 @@ class MonitoringResultsController < ApplicationController
 
 
   def index
+  	@filter_form = params["filter"] || {}
 
-  	@monitoring = MonitoringResult.first
- 
-  	
-    @pie_chart_controllers = @monitoring.result.keys
-    @pie_chart_controllers.delete_if{|el| ["controllers", "severity"].include?(el)}
+  	@monitoring_days = MonitoringResult.pluck("monitoring_day")
+
+  	@monitoring = MonitoringResult.where(nil)
+
+  	if @filter_form["monitoring_day"]
+      @monitoring = MonitoringResult.where("monitoring_day = ?", @filter_form["monitoring_day"])
+  	end
+
+    @monitoring = @monitoring.first
+
+    @all_controllers  = []
     @controllers_list = []
+
+  	unless @monitoring.nil?
+  	  @all_controllers = @monitoring.result.keys
+      @all_controllers.delete_if{|el| ["controllers", "severity"].include?(el)}
+  	end
+    if !@filter_form["controllers"].blank? and !@filter_form["controllers"].first.blank?
+      @controllers_list = @filter_form["controllers"]
+    end
+    
   end
 end
