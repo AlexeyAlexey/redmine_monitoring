8a0619ec164d1ffafb8caf4e709838aa064480a1
Alexey Kondratenko <a.kondratenko@smileexpo.ru>
2016-11-25 15:02:45 +0200

    fixed bug in migration

--- db/migrate/001_create_monitoring_results.rb
+++ db/migrate/001_create_monitoring_results.rb
@@ -6,8 +6,6 @@ class CreateMonitoringResults < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-    add_index "monitoring_results", [:project_id]
-    add_index "monitoring_results", [:server_id]
   end
 
   def down
